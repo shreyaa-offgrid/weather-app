fbb01c82ebd26ea6770747c1a71c08c977bb89ab
Shreya Singh <143871325+shreyaa-offgrid@users.noreply.github.com>
2026-01-11 07:51:39 +0000

    Rename api.js to weatherAPI.js

--- src/theme.js
+++ src/theme.js
@@ -1,1 +1,10 @@
-//giphy go brr
+export async function getGif(searchTerm){
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=llaL0IkVsCwI3NBQR14A6toKeFhHnA6J&q=${searchTerm}+sky&limit=1&offset=1&rating=g&lang=en&bundle=messaging_non_clips`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const gifUrl = data.data.images.original.url;
+    if(!gifUrl){
+        throw new Error("No GIF found for that search");
+    }
+    return gifUrl;
+}
